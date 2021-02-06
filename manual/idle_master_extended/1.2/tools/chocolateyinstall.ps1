68c02331c8735041015d90b1ff347e0b7634c53d
ALazyMeme <jkeasley@icloud.com>
2021-02-06 20:38:39 +1000

    Fix shortcut

--- manual/idle_master_extended/1.2/tools/chocolateyinstall.ps1
+++ manual/idle_master_extended/1.2/tools/chocolateyinstall.ps1
@@ -4,4 +4,4 @@ $fileLocation = Join-Path $toolsDir 'idle-master-extended.zip'
 $packagename  = 'Idle Master Extended'
 
 Get-ChocolateyUnzip -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\idle-master-extended.zip" -Destination "C:\Program Files\Idle Master Extended" -PackageName "Idle Master Extended"
-Install-ChocolateyShortcut -ShortcutFilePath "$env:USERPROFILE\Desktop\Idle Master Extended.lnk" -TargetPath "C:\Program Files\Idle Master Extended\IdleMasterExtended.exe"
+Install-ChocolateyShortcut -ShortcutFilePath "$env:USERPROFILE\Desktop\Idle Master Extended.lnk" -TargetPath "C:\Program Files\Idle Master Extended\IdleMaster.exe"
