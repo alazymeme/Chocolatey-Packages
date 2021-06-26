bb25d9c5dac1ffabecfb46b4dcdbdf996d43c86f
ALazyMeme <jkeasley@icloud.com>
2021-06-27 01:40:26 +1000

    Update chatterino-fortnightly install file

--- automatic/chatterino/fortnightly/tools/chocolateyinstall.ps1
+++ automatic/chatterino/fortnightly/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'chatterino-fortnightly.zip'
+$url          = 'https://github.com/Chatterino/chatterino2/releases/download/nightly-build/chatterino-windows-x86-64.zip'
 $packagename  = 'Chatterino'
 
 Get-ChocolateyUnzip -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\chatterino-fortnightly.zip" -Destination "C:\Program Files\Chatterino" -PackageName "Chatterino"
