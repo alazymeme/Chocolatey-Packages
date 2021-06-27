b7436f4ddbeadf0ad9c5f2cb87fc086d5a3caa94
ALazyMeme <jkeasley@icloud.com>
2021-06-27 11:22:03 +1000

    Move shit + add dankerino

---
+++ manual/dankerino/stable/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'chatterino-installer.exe'
+$packagename  = 'Chatterino'
+
+$packageArgs = @{
+  packageName   = $packagename
+  fileType      = 'EXE'
+  softwareName  = 'Chatterino*'
+  file			= $fileLocation
+  validExitCodes= @(0)
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /MERGETASKS="!vcredist"'
+  checksum      = '1081456CC491C9B29CFE45DA00741698A464FD5B06537A2DF213026E015D7C19'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+Install-ChocolateyShortcut -ShortcutFilePath "$env:USERPROFILE\Desktop\Chatterino.lnk" -TargetPath "C:\Program Files\Chatterino\chatterino.exe"
