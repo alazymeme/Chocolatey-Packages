5753afb1f70c7978dae8094a8cb3ecdca54f68d7
ALazyMeme <jkeasley@icloud.com>
2021-07-14 20:33:35 +1000

    Added v2.3.3-beta

--- manual/chatterino/beta/tools/chocolateyinstall.ps1
+++ manual/chatterino/beta/tools/chocolateyinstall.ps1
@@ -1,7 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'chatterino-beta.zip'
+$fileLocation = Join-Path $toolsDir 'chatterino-installer.exe'
 $packagename  = 'Chatterino'
 
-Get-ChocolateyUnzip -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\chatterino-beta.zip" -Destination "C:\Program Files\Chatterino" -PackageName "Chatterino"
+$packageArgs = @{
+  packageName   = $packagename
+  fileType      = 'EXE'
+  softwareName  = 'Chatterino*'
+  file			= $fileLocation
+  validExitCodes= @(0)
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /MERGETASKS="!vcredist"'
+  checksum      = '1081456CC491C9B29CFE45DA00741698A464FD5B06537A2DF213026E015D7C19'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
 Install-ChocolateyShortcut -ShortcutFilePath "$env:USERPROFILE\Desktop\Chatterino.lnk" -TargetPath "C:\Program Files\Chatterino\chatterino.exe"
